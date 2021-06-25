e068d785dde0f0f400f95d7f9d6ed4f0e902a6e2
kisun <kisun168@aliyun.com>
2021-06-25 15:51:36 +0800

    bug: linux版本不能调用dll，处理报错，临时注释dll相关代码

--- src/core/syscall.go
+++ src/core/syscall.go
@@ -4,32 +4,26 @@
 
 package core
 
-import (
-	"errors"
-	"fmt"
-	"syscall"
-)
-
-func CallDLLProc(DLLPath string, proc string, args ...uintptr) (r1, r2 uintptr, err error) {
-	defer func() {
-		if e := recover(); e != nil {
-			r1, r2, err = 0, 0, errors.New(fmt.Sprintf("error in CallDLLProc: %v", e))
-		}
-	}()
-
-	var DLL *syscall.DLL
-	var fProc *syscall.Proc
-
-	DLL, err = syscall.LoadDLL(DLLPath)
-	if err != nil {
-		return 0, 0, err
-	}
-
-	fProc, err = DLL.FindProc(proc)
-	if err != nil {
-		return 0, 0, err
-	}
-
-	r1, r2, _ = fProc.Call(args...)
-	return r1, r2, nil
-}
+//func CallDLLProc(DLLPath string, proc string, args ...uintptr) (r1, r2 uintptr, err error) {
+//	defer func() {
+//		if e := recover(); e != nil {
+//			r1, r2, err = 0, 0, errors.New(fmt.Sprintf("error in CallDLLProc: %v", e))
+//		}
+//	}()
+//
+//	var DLL *syscall.DLL
+//	var fProc *syscall.Proc
+//
+//	DLL, err = syscall.LoadDLL(DLLPath)
+//	if err != nil {
+//		return 0, 0, err
+//	}
+//
+//	fProc, err = DLL.FindProc(proc)
+//	if err != nil {
+//		return 0, 0, err
+//	}
+//
+//	r1, r2, _ = fProc.Call(args...)
+//	return r1, r2, nil
+//}
